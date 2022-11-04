9fdab2a1e5a270700571d64721e4bedc938c3461
Michael Ehab <michaelehab16@gmail.com>
2022-11-04 18:22:28 +0200

    Added Profile button in navbar

--- web/src/components/navBar.tsx
+++ web/src/components/navBar.tsx
@@ -1,12 +1,13 @@
 import { Flex, Box, Button, Image } from "@chakra-ui/react";
 import Logo from "../assets/logo/svg/logo-no-background.svg";
 import { Link } from "react-router-dom";
-import { isLoggedIn, signOut } from "../utils/auth";
+import { getLocalStorageUserId, isLoggedIn, signOut } from "../utils/auth";
 import { useNavigate } from "react-router-dom";
 import { useCallback } from "react";
 
 export const NavBar = () => {
   const navigate = useNavigate();
+  const userId: string = getLocalStorageUserId();
 
   const onSignout = useCallback(() => {
     signOut();
@@ -22,6 +23,11 @@ export const NavBar = () => {
       <Flex gap={3}>
         {isLoggedIn() ? (
           <>
+            <Link to={`/user/${userId}`}>
+              <Button colorScheme="green" variant="outline">
+                Profile
+              </Button>
+            </Link>
             <Link to={"/new/post"}>
               <Button colorScheme="green" variant="solid">
                 New post
